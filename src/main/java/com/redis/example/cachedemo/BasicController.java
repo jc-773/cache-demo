c2657f9247f09e13cc4810cc1b9ba105441edef3
jc-773 <jonc960@gmail.com>
2024-04-30 21:15:48 -0400

    clean

--- src/main/java/com/redis/example/cachedemo/BasicController.java
+++ src/main/java/com/redis/example/cachedemo/BasicController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
